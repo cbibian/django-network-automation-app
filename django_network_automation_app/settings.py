281571ae3d4464d27ff9545ab5e2dbde30b8dfde
Charles Bibian <cbibiaan@gmail.com>
2026-02-26 04:52:33 -0800

    add database django information

--- django_network_automation_app/settings.py
+++ django_network_automation_app/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'django_network_automation_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "nacc",
+        "USER": "nacc_user",
+        "PASSWORD": "admin123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
