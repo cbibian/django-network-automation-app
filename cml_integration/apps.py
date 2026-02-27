ab0480dfb3d955758bbf1dcf7b75ab898fa125c1
Charles Bibian <cbibiaan@gmail.com>
2026-02-27 03:25:55 -0800

    added cml_integration, modified urls.py to include router, device, and jobs views

---
+++ cml_integration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CmlIntegrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cml_integration'
--- django_network_automation_app/urls.py
+++ django_network_automation_app/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from devices.views import DeviceViewSet
+from jobs.views import PlaybookJobViewSet
+from cml_integration.views import CMLSettingsViewSet
+
+router = DefaultRouter()
+router.register(r"devices", DeviceViewSet, basename="devices")
+router.register(r"jobs", PlaybookJobViewSet, basename="jobs")
+router.register(r"cml-settings", CMLSettingsViewSet, basename="cml-settings")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
