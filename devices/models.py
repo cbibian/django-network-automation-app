dc40808c200736e76414ef8fb1c9874c1636d957
Charles Bibian <cbibiaan@gmail.com>
2026-02-28 21:20:58 -0800

    added devices/model to devices/admin

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class DeviceGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Device(models.Model):
+    hostname = models.CharField(max_length=100, unique=True)
+    mgmt_ip = models.GenericIPAddressField()
+    os_type = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    group = models.ForeignKey(DeviceGroup, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.hostname
+
