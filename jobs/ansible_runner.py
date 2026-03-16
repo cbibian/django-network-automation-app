060ad33bf48f9ae6c792fbd8fa878ed057be7581
Charles Bibian <cbibiaan@gmail.com>
2026-03-16 00:48:23 -0700

    corrected inventory.ini and ansible-runner group settings to post from Cisco CML lab nodes; updated requirements.txt

--- jobs/ansible_runner.py
+++ jobs/ansible_runner.py
@@ -6,11 +6,21 @@ def run_playbook(job_id):
     job = PlaybookJob.objects.select_related("device").get(id=job_id)
     device = job.device
 
-    inventory_path = Path("inventory.ini")
-    inventory_path.write_text(f"""
-[{device.group.name if device.group else "devices"}]
+    group_name = device.group.name if device.group else "devices"
+
+    inventory_content = f"""
+[{group_name}]
 {device.hostname} ansible_host={device.mgmt_ip} ansible_user={device.username} ansible_password={device.password}
-""")
+
+[{group_name}:vars]
+nsible_connection=network_cli
+ansible_network_os=ios
+ansible_network_cli_ssh_type=paramiko
+ansible_ssh_extra_args=-oKexAlgorithms=+diffie-hellman-group14-sha1 -oHostKeyAlgorithms=+ssh-rsa -oPubkeyAcceptedAlgorithms=+ssh-rsa -oStrictHostKeyChecking=no
+"""
+
+    inventory_path = Path("inventory.ini")
+    inventory_path.write_text(inventory_content)
 
     cmd = [
         "ansible-playbook",
